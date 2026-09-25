stablyai/orca

feat(editor): add LSP support to the editor and diff views
Spawn user-installed language servers (tsgo-first for TS/JS, plus
typescript-language-server, pyright, gopls, rust-analyzer) per local
workspace root over stdio, and wire Monaco hover, go-to-definition,
find-references, completions, and push+pull diagnostics in both the
file editor and the modified pane of diff views. A status-bar segment
shows the attached server for the active file. Remote/SSH/web surfaces
and machines without a server on PATH degrade silently to today's
behavior; nothing is bundled.

The sandboxed TS worker's hover/completion/definition/references are
disabled — the same reasoning as its already-disabled diagnostics: it
collapses imports to `any` and misleads. Features the bridge does not
replace (signature help, rename, code actions, outline, formatting)
stay on the worker.

Co-Authored-By: Claude Fable 5 <noreply@anthropic.com>

src/main/lsp/lsp-session-state.ts
@@ -1,0 +1,33 @@
+import type { ChildProcessWithoutNullStreams } from 'node:child_process'
+
+export type PendingRequest = {
+  resolve: (result: unknown) => void
+  reject: (error: Error) => void
+  timer: NodeJS.Timeout
+}
+
+// Why: documentsByUri is keyed by canonicalFileUriKey; keep the exact URI the
+// client knows so diagnostics forwarded to the renderer match its lookups.
+export type OpenDocumentState = { fileUri: string; version: number; refCount: number }
+
+export type LspSession = {
+  sessionId: string
+  key: string
+  serverId: string
+  child: ChildProcessWithoutNullStreams
+  nextRequestId: number
+  pending: Map<number, PendingRequest>
+  documentsByUri: Map<string, OpenDocumentState>
+  initialization: Promise<void>
+  // Why: pull-model servers (tsgo, TS7) advertise diagnosticProvider and never
+  // push; the renderer must know which model this session speaks.
+  pullDiagnostics: boolean
+  idleTimer: NodeJS.Timeout | null
+  disposed: boolean
+}
+
+export type LspDiagnosticsListener = (payload: {
+  sessionId: string
+  fileUri: string
+  diagnostics: unknown[]
+}) => void

Old-G/orca-plus

Custom build: cherry-picked from upstream PR stablyai/orca#14873
(by Moishi Netzer), rebased onto current main. Conflict resolution:
- preload: the api moved into per-domain bridges, so the lsp api lives
  in src/preload/api/lsp-bridge.ts (two lines in preload/index.ts)
- status bar: segment added in StatusBarSurface.tsx (where segments render now)
- DiffViewer: only the useMonacoLspForDiff hook; the PR's incidental
  comment-submit extraction is dropped to keep the upstream touch minimal
- en.json: LspStatusSegment keys next to upstream's new segments
Custom-Feature: lsp
Co-Authored-By: Claude Opus 5.5 (1M context) <noreply@anthropic.com>

src/main/lsp/lsp-session-state.ts
@@ -1,0 +1,33 @@
+import type { LspServerProcess } from './lsp-server-spawn'
+
+export type PendingRequest = {
+  resolve: (result: unknown) => void
+  reject: (error: Error) => void
+  timer: NodeJS.Timeout
+}
+
+// Why: documentsByUri is keyed by canonicalFileUriKey; keep the exact URI the
+// client knows so diagnostics forwarded to the renderer match its lookups.
+export type OpenDocumentState = { fileUri: string; version: number; refCount: number }
+
+export type LspSession = {
+  sessionId: string
+  key: string
+  serverId: string
+  child: LspServerProcess
+  nextRequestId: number
+  pending: Map<number, PendingRequest>
+  documentsByUri: Map<string, OpenDocumentState>
+  initialization: Promise<void>
+  // Why: pull-model servers (tsgo, TS7) advertise diagnosticProvider and never
+  // push; the renderer must know which model this session speaks.
+  pullDiagnostics: boolean
+  idleTimer: NodeJS.Timeout | null
+  disposed: boolean
+}
+
+export type LspDiagnosticsListener = (payload: {
+  sessionId: string
+  fileUri: string
+  diagnostics: unknown[]
+}) => void